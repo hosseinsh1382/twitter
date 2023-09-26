d228a80d27bd8c9409227c379b42f29b2f91c63a
Hossein Shirkavand <hosseinshirkavand82@gmail.com>
2023-09-26 14:09:42 +0330

    Post added

---
+++ Twitter/Models/Post.cs
@@ -0,0 +1,13 @@
+﻿namespace Twitter.Models;
+
+public class Post
+{
+    public Guid Id { get; set; }
+    public string Text { get; set; }
+    public IEnumerable<Account>? Viewrs { get; set; }
+    public IEnumerable<Account>? Like { get; set; }
+    public DateTime PublishDateTime { get; set; }
+
+    public int AccountId { get; set; }
+    public Account? Account { get; set; }
+}
