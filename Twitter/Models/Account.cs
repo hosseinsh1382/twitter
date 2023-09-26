4b5e4bdd56e4912379399452691408b7f596d585
Hossein Shirkavand <hosseinshirkavand82@gmail.com>
2023-09-26 12:25:02 +0330

    Account added

---
+++ Twitter/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿namespace Twitter.Models;
+
+public class Account
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string AccountName { get; set; }
+    public string Password { get; set; }
+    public string? Fullname { get; set; }
+    public string Email { get; set; }
+    public string? Biography { get; set; }
+    public DateTime JoinedDate { get; set; }
+    public List<Account>? Following { get; set; }
+    public List<Account>? Follower { get; set; }
+}
