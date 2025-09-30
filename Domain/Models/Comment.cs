ceeb13f7ecab3a5e5a7cd25f05c399feb7cd00ac
Hossein <hosseinshirkavand82@gmail.com>
2025-09-30 11:35:12 +0330

    refactor models

--- Domain/Models/Comment.cs
+++ Domain/Models/Comment.cs
@@ -1,12 +1,9 @@
-﻿namespace Twitter.Models;
+﻿namespace Domain.Models;
 
 public class Comment
 {
     public Guid Id { get; set; }
-    
     public string Text { get; set; } = string.Empty;
-    
-    public Guid PostId { get; set; }
-    
-    public Guid AccountId { get; set; }
+    public required Guid PostId { get; set; }
+    public required Guid AccountId { get; set; }
 }
--- Domain/Models/Post.cs
+++ Domain/Models/Post.cs
@@ -1,14 +1,13 @@
-﻿namespace Twitter.Models;
+﻿namespace Domain.Models;
 
 public class Post
 {
     public Guid Id { get; set; }
-    public string Text { get; set; }
-    public IEnumerable<Account> Viewers { get; set; } = Enumerable.Empty<Account>();
-    public IEnumerable<Account> Likes { get; set; } = Enumerable.Empty<Account>();
-    public IEnumerable<Comment> Comments { get; set; } = Enumerable.Empty<Comment>();
-    public DateTime PublishDateTime { get; set; }
-
-    public Guid AccountId { get; set; }
-    public Account? Account { get; set; }
+    public required string Text { get; set; }
+    public IEnumerable<User> Viewers { get; set; } = [];
+    public IEnumerable<User> Likes { get; set; } = [];
+    public IEnumerable<Comment> Comments { get; set; } = [];
+    public DateTime PublishDateTime { get; set; } = DateTime.UtcNow;
+    public required Guid PublisherId { get; set; }
+    public User? Publisher { get; set; }
 }
